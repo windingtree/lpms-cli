1c2affe85c5f33c98744921c61916ee2b1f72506
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-06-22 21:18:31 +0200

    refactor: 💡 removed `metadata` option from `storage` command

--- src/api/serviceProvider.ts
+++ src/api/serviceProvider.ts
@@ -12,19 +12,18 @@ import { genRole } from '../utils/roles';
 export const getServiceProviderIdLocal = (
   salt: string,
   address: string
-): string => {
-  const encoder = new utils.AbiCoder();
-  return utils.solidityKeccak256(
-    ['bytes'],
-    [encoder.encode(['bytes32', 'address'], [salt, address])]
-  );
-};
+): string => utils.keccak256(
+  utils.defaultAbiCoder.encode(
+    // Do **NOT** use solidityPack due to abi coder differences.
+    ['bytes32', 'address'],
+    [salt, address]
+  )
+);
 
 export const getServiceProviderId = (
   contract: ServiceProviderRegistry,
-  salt: string,
-  metadataUri: string
-): Promise<string> => contract.callStatic.enroll(salt, metadataUri);
+  salt: string
+): Promise<string> => contract.callStatic.enroll(salt);
 
 export const getServiceProviderRegistryContract = (
   wallet: Wallet
@@ -46,54 +45,30 @@ export const getLineRegistryContract = (
   )
 }
 
-export const registerServiceProvider = async (
-  serviceProviderRegistryContract: ServiceProviderRegistry,
-  lineRegistryContract: LineRegistry,
-  salt: string,
-  metadataUri: string,
-  spinnerCallback: SpinnerCallback,
-  options?: CallOverrides
-): Promise<string> => {
+export const getMappedAddresses = async (): Promise<Record<number, string>> => {
   const addresses = await getAddresses();
-  const addressesMap = addresses.reduce(
+  return addresses.reduce(
     (a, v) => ({
       ...a,
       [Number(v.id)]: v.address
     }),
     {}
   );
+};
 
-  const serviceProviderId = utils.keccak256(
-    utils.defaultAbiCoder.encode(
-      // Do **NOT** use solidityPack due to abi coder differences.
-      ['bytes32', 'address'],
-      [salt, await serviceProviderRegistryContract.signer.getAddress()]
-    )
+export const registerServiceProvider = async (
+  serviceProviderRegistryContract: ServiceProviderRegistry,
+  lineRegistryContract: LineRegistry,
+  salt: string,
+  spinnerCallback: SpinnerCallback,
+  options?: CallOverrides
+): Promise<string> => {
+  const addressesMap = await getMappedAddresses();
+
+  const serviceProviderId = getServiceProviderIdLocal(
+    salt,
+    await serviceProviderRegistryContract.signer.getAddress()
   );
-
-  // spinnerCallback(`Granting ${addressesMap[Role.API]} the API role`);
-  // const VIDERE_API_ROLE = genRole(serviceProviderId, Role.API);
-  // if (!(await contract.hasRole(VIDERE_API_ROLE, addressesMap[Role.API - 1]))) {
-  //   await contract.grantRole(VIDERE_API_ROLE, addressesMap[Role.API - 1], options)
-  // }
-
-  // spinnerCallback(`Granting ${addressesMap[Role.BIDDER]} the BIDDER role`);
-  // const VIDERE_BIDDER_ROLE = genRole(serviceProviderId, Role.BIDDER);
-  // if (!(await contract.hasRole(VIDERE_BIDDER_ROLE, addressesMap[Role.BIDDER - 1]))) {
-  //   await contract.grantRole(VIDERE_BIDDER_ROLE, addressesMap[Role.BIDDER - 1], options)
-  // }
-
-  // spinnerCallback(`Granting ${addressesMap[Role.MANAGER]} the MANAGER role`);
-  // const VIDERE_MANAGER_ROLE = genRole(serviceProviderId, Role.MANAGER);
-  // if (!(await contract.hasRole(VIDERE_MANAGER_ROLE, addressesMap[Role.MANAGER - 1]))) {
-  //   await contract.grantRole(VIDERE_MANAGER_ROLE, addressesMap[Role.MANAGER - 1], options)
-  // }
-
-  // spinnerCallback(`Granting ${addressesMap[Role.STAFF]} the STAFF role`);
-  // const VIDERE_STAFF_ROLE = genRole(serviceProviderId, Role.STAFF);
-  // if (!(await contract.hasRole(VIDERE_STAFF_ROLE, addressesMap[Role.STAFF - 1]))) {
-  //   await contract.grantRole(VIDERE_STAFF_ROLE, addressesMap[Role.STAFF - 1], options)
-  // }
 
   if (!await serviceProviderRegistryContract.exists(serviceProviderId)) {
     spinnerCallback('Registering of the service provider');
@@ -104,7 +79,7 @@ export const registerServiceProvider = async (
         // enroll
         ServiceProviderRegistry__factory.createInterface().encodeFunctionData(
           'enroll',
-          [salt, metadataUri]
+          [salt]
         ),
         // api-role
         ServiceProviderRegistry__factory.createInterface().encodeFunctionData(
@@ -136,7 +111,7 @@ export const registerServiceProvider = async (
       options
     );
 
-    // wait for confirmation
+    // wait for confirmations
     await tx.wait(1);
   }
 
@@ -151,27 +126,8 @@ export const registerServiceProvider = async (
   return serviceProviderId;
 };
 
-export const updateServiceProvider = async (
-  contract: ServiceProviderRegistry,
-  serviceProviderId: string,
-  metadataUri: string,
-  spinnerCallback: SpinnerCallback,
-  options?: CallOverrides
-): Promise<void> => {
-  spinnerCallback('Updating the dataURI of the service provider...');
-  const tx = await contract['file(bytes32,bytes32,string)'](
-    serviceProviderId,
-    utils.formatBytes32String('dataURI'),
-    metadataUri,
-    options
-  );
-
-  // don't exit until transaction is confirmed.
-  await tx.wait(1);
-};
-
 export const serviceProviderController: ActionController = async (
-  { salt, id, meta, register, update, reset, gasPrice },
+  { salt, id, register, reset, gasPrice },
   program
 ) => {
   const spinner = ora('Registering of the service provider...');
@@ -180,7 +136,6 @@ export const serviceProviderController: ActionController = async (
     if (reset) {
       removeConfig('serviceProviderId');
       removeConfig('salt');
-      removeConfig('metadataUri');
       yellow('Information about the service provider is wiped out');
       return;
     }
@@ -191,7 +146,8 @@ export const serviceProviderController: ActionController = async (
       getConfig('defaultAccountIndex') as number
     );
     const owner = await wallet.getAddress();
-    const serviceProviderRegistryContract = getServiceProviderRegistryContract(wallet);
+    const serviceProviderRegistryContract =
+      getServiceProviderRegistryContract(wallet);
     const lineRegistryContract = getLineRegistryContract(wallet);
 
     if (!salt) {
@@ -200,14 +156,12 @@ export const serviceProviderController: ActionController = async (
     }
 
     if (id) {
-      const localId = getServiceProviderIdLocal(salt, owner);
+      const localId = getServiceProviderIdLocal(
+        salt,
+        await serviceProviderRegistryContract.signer.getAddress()
+      );
       green(`Service provider Id: ${localId}`);
       return;
-    }
-
-    if (!meta) {
-      requiredConfig(['metadataUri']);
-      meta = getConfig('metadataUri') as string;
     }
 
     let serviceProviderId: string;
@@ -227,7 +181,6 @@ export const serviceProviderController: ActionController = async (
         serviceProviderRegistryContract,
         lineRegistryContract,
         salt,
-        meta,
         (text) => {
           spinner.text = text;
         },
@@ -235,7 +188,6 @@ export const serviceProviderController: ActionController = async (
       );
 
       saveConfig('salt', salt);
-      saveConfig('metadataUri', meta);
       saveConfig('serviceProviderId', serviceProviderId);
 
       spinner.stop();
@@ -243,41 +195,15 @@ export const serviceProviderController: ActionController = async (
       green(
         `Service provider with Id: ${serviceProviderId} has been registered successfully`
       );
-    } else if (update) {
-      requiredConfig(['serviceProviderId']);
-
-      spinner.start();
-
-      serviceProviderId = getConfig('serviceProviderId') as string;
-      await updateServiceProvider(
-        serviceProviderRegistryContract,
-        serviceProviderId,
-        meta,
-        (text) => {
-          spinner.text = text;
-        },
-        txOptions
-      );
-
-      saveConfig('metadataUri', meta);
-
-      spinner.stop();
-
-      green(
-        `Service provider with Id: ${serviceProviderId} has been updated successfully`
-      );
-      green(`The new "dataURI" is: ${meta}`);
     } else {
       requiredConfig(['serviceProviderId']);
       serviceProviderId = getConfig('serviceProviderId') as string;
       salt = getConfig('salt') as string;
-      meta = await serviceProviderRegistryContract.datastores(serviceProviderId);
 
       spinner.stop();
 
       green(`Service provider Id: ${serviceProviderId}`);
       green(`Unique salt string: ${salt}`);
-      green(`Metadata storage Id: ${meta}`);
       green(`Owner address: ${owner}`);
     }
   } catch (error) {

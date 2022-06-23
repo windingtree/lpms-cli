0d28bf84eb2e710700e3ae6c78996dfdfa9609f4
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-06-23 14:06:05 +0200

    feat: 🎸 implemented `facility` command (new version)
    Getting, adding/updating of metadata, activation, deactivation, removal

--- src/api/facility.ts
+++ src/api/facility.ts
@@ -1,20 +1,99 @@
 import type {
-  ActionController,
-  ApiSuccessResponse
+  Facility
+} from '@windingtree/stays-models/dist/cjs/proto/facility';
+import type {
+  ActionController
 } from '../types';
-import ora from 'ora';
+import { promises as fs } from 'fs';
+import ora, { Ora } from 'ora';
 import axios from 'axios';
 import { getConfig, requiredConfig } from './config';
 import { getAuthHeader } from './login';
-import { green } from '../utils/print';
+import { green, printObject } from '../utils/print';
+import { saveToFile } from '../utils/files';
 
-export const toggleFacility = async (
+export const getMetadata = async (
   facilityId: string,
-  operationKind: boolean
-): Promise<ApiSuccessResponse> => {
+  spinner: Ora
+): Promise<Facility> => {
   requiredConfig(['apiUrl']);
 
   const authHeader = await getAuthHeader();
+
+  spinner.start();
+  spinner.text = `Getting the metadata of the facility: ${facilityId}...`
+
+  const { data } = await axios.get(
+    `${getConfig('apiUrl')}/api/facility/${facilityId}`,
+    {
+      headers: authHeader
+    }
+  );
+
+  spinner.stop();
+
+  green(`Facility ${facilityId} metadata:`);
+  printObject(data);
+
+  return data as Facility;
+};
+
+export const updateMetadata = async (
+  facilityId: string,
+  metadataPath: string,
+  spinner: Ora
+): Promise<void> => {
+  requiredConfig(['apiUrl']);
+
+  const authHeader = await getAuthHeader();
+  let metadata: undefined | Facility;
+
+  spinner.start();
+  spinner.text = `Reading the metadata from ${metadataPath}`;
+
+  try {
+    const fileBuffer = await fs.readFile(metadataPath);
+    metadata = JSON.parse(fileBuffer.toString()) as Facility;
+  } catch (e) {
+    throw new Error(`Unable to read metadata from file`);
+  }
+
+  spinner.text = `Uploading ${metadataPath}`;
+
+  const { data } = await axios.post(
+    `${getConfig('apiUrl')}/api/facility/${facilityId}`,
+    metadata,
+    {
+      headers: {
+        ...authHeader
+      }
+    }
+  );
+
+  spinner.stop();
+
+  if (!data.success) {
+    throw new Error(`Something went wrong during the update of the metadata`);
+  }
+
+  green(
+    `Metadata of the facility ${facilityId} has been updated successfully`
+  );
+};
+
+export const toggleFacility = async (
+  facilityId: string,
+  operationKind: boolean,
+  spinner: Ora
+): Promise<void> => {
+  requiredConfig(['apiUrl']);
+
+  const authHeader = await getAuthHeader();
+
+  spinner.start();
+  spinner.text = `${
+    operationKind ? 'Activating' : 'Deactivating'
+  } the facility: ${facilityId}`;
 
   const { data } = await axios.post(
     `${getConfig(
@@ -28,14 +107,57 @@ export const toggleFacility = async (
     }
   );
 
-  return data;
+  spinner.stop();
+
+  if (!data.success) {
+    throw new Error(
+      `Something went wrong during toggling state of the facility`
+    );
+  }
+
+  green(
+    `The facility: ${facilityId} has been ${
+      operationKind ? 'activated' : 'deactivated'
+    } successfully`
+  );
+};
+
+export const removeFacility = async (
+  facilityId: string,
+  spinner: Ora
+): Promise<void> => {
+  requiredConfig(['apiUrl']);
+
+  const authHeader = await getAuthHeader();
+
+  spinner.start();
+  spinner.text = `Removing of the facility: ${facilityId}...`
+
+  const { data } = await axios.delete(
+    `${getConfig('apiUrl')}/api/facility/${facilityId}`,
+    {
+      headers: authHeader
+    }
+  );
+
+  spinner.stop();
+
+  if (!data.success) {
+    throw new Error(
+      `Something went wrong during removal of the facility`
+    );
+  }
+
+  green(
+    `The facility ${facilityId} has been removed successfully`
+  );
 };
 
 export const facilityController: ActionController = async (
-  { facilityId, activate, deactivate },
+  { facilityId, out, activate, deactivate, metadata, remove },
   program
 ) => {
-  const spinner = ora('Running the facility operation...');
+  const spinner = ora('Running the facility management operation...');
 
   try {
     if (!facilityId) {
@@ -44,29 +166,38 @@ export const facilityController: ActionController = async (
       );
     }
 
+    if (!activate && !deactivate && !metadata && !remove) {
+      // No options provided, so, just get the facility metadata from the server
+      const data = await getMetadata(facilityId, spinner);
+
+      if (out) {
+        await saveToFile(out, data, spinner);
+      }
+
+      return;
+    }
+
+    if (metadata) {
+      // save metadata of the facility
+      await updateMetadata(facilityId, metadata, spinner);
+    }
+
     if (activate && deactivate) {
       throw new Error(
         'You cannot use --activate and --deactivate options together'
       );
     }
 
+    // Toggling of the active status of the facility
     if (activate) {
-      spinner.start();
-      spinner.text = `Activating the facility: ${facilityId}`
-      await toggleFacility(facilityId, true);
-      green(
-        `The facility: ${facilityId} has been activated successfully`
-      );
+      await toggleFacility(facilityId, true, spinner);
     } else if (deactivate) {
-      spinner.start();
-      spinner.text = `Deactivating the facility: ${facilityId}`
-      await toggleFacility(facilityId, false);
-      green(
-        `The facility: ${facilityId} has been deactivated successfully`
-      );
+      await toggleFacility(facilityId, false, spinner);
     }
 
-    spinner.stop();
+    if (remove) {
+      await removeFacility(facilityId, spinner);
+    }
   } catch (error) {
     spinner.stop();
     program.error(error, { exitCode: 1 });
--- src/index.ts
+++ src/index.ts
@@ -27,16 +27,16 @@ program
 program
   .command('config')
   .description('Adds or removes configuration properties')
-  .option('--get <property>', 'view a specific property value')
-  .option('--add <property>', 'specify a property to add')
-  .option('--value <value>', 'specify a property value to add')
-  .option('--remove <property>', 'specify a property to remove from config')
+  .option('--get <property>', 'View a specific property value')
+  .option('--add <property>', 'Specify a property to add')
+  .option('--value <value>', 'Specify a property value to add')
+  .option('--remove <property>', 'Specify a property to remove from config')
   .action(configController);
 
 program
   .command('mnemonic')
   .description('Generates random 24 word mnemonic')
-  .option('--save', 'save generated mnemonic to config')
+  .option('--save', 'Save generated mnemonic to config')
   .option(
     '--index <index>',
     'specifies the default account index. "0" by default'
@@ -46,7 +46,7 @@ program
 program
   .command('salt')
   .description('Returns a random salt string (bytes32)')
-  .option('--save', 'save generated salt to config')
+  .option('--save', 'Save generated salt to config')
   .action(saltController);
 
 program
@@ -56,20 +56,20 @@ program
     '--index <index>',
     'specifies an account index to show. "0" by default'
   )
-  .option('--keys', 'export public and private keys of account by index')
+  .option('--keys', 'Export public and private keys of account by index')
   .action(walletController);
 
 program
   .command('login')
   .description('Makes login with password')
-  .option('--login <login>', 'specify the login')
-  .option('--password <password>', 'specify the password')
+  .option('--login <login>', 'Specify the login')
+  .option('--password <password>', 'Specify the password')
   .action(loginController);
 
 program
   .command('storage')
   .description('Uploads files to storage')
-  .option('--file <path>', 'specify a path to the file')
+  .option('--file <path>', 'Specify a path to the file')
   .action(storageController);
 
 program
@@ -84,35 +84,43 @@ program
     '--id',
     'returns a service provider Id based on salt and signer address'
   )
-  .option('--salt <salt>', 'specify a salt string')
-  .option('--register', 'initiate registration of service provider')
+  .option('--salt <salt>', 'Specify a salt string')
+  .option('--register', 'Initiate registration of service provider')
   .option(
     '--reset',
     'wipe saved information about the registered service provider'
   )
-  .option('--gasPrice <value>', 'gas price in wei')
+  .option('--gasPrice <value>', 'Gas price in wei')
   .action(serviceProviderController);
 
 program
   .command('facility')
   .description('Operation with the facility')
-  .option('--facilityId <value>', 'specifies the facility Id')
+  .option('--facilityId <value>', 'Specify the facility Id')
+  .option('--out <path>', 'Path of the file to save')
   .option('--activate', 'Activates the facility')
   .option('--deactivate', 'Deactivates the facility')
+  .option('--metadata <path>', 'Path to metadata file to add/update')
+  .option('--remove', 'Remove the facility')
   .action(facilityController)
 
 program
   .command('space')
   .description('Operations with a space')
   .option('--facilityId <value>', 'specifies the facility Id')
-  .option('--spaceId <value>', 'specifies the space Id')
-  .option('--get', 'get operation modifier')
-  .option('--add', 'add/update operation modifier')
+  .option('--spaceId <value>', 'Specify the space Id')
+  .option('--out <path>', 'Path of the file to save')
+  .option('--metadata <path>', 'Path to metadata file to add/update')
   .option(
     '--availability <type>',
     'specify availability-related type of operation'
   )
   .option('--numSpaces <value>', 'specifies number of available spaces')
+  .option('--get', 'get operation modifier (when manage availability, etc)')
+  .option(
+    '--add',
+    'add/update operation modifier (when manage availability, etc)'
+  )
   .action(spaceController);
 
 program.parse();
